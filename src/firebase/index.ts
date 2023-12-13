c8f449b4a9bc09b29fc45d7e1570fb09d47dd8fc
Sattwik Sahu <sahusattwik@gmail.com>
2023-12-13 13:21:00 +0530

    add:firebase hosting

---
+++ src/firebase/index.ts
@@ -0,0 +1,21 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCeEuduMbEOljzZ1jX9M98AdDrCJQZVoU8",
+//   authDomain: "iiserllm.firebaseapp.com",
+//   projectId: "iiserllm",
+//   storageBucket: "iiserllm.appspot.com",
+//   messagingSenderId: "889823312756",
+//   appId: "1:889823312756:web:bd3db5befbaab992e04a21",
+//   measurementId: "G-5V2V7EP1GK",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
