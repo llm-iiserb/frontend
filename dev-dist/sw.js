e9304e7ba9d91508f389683809321699a79a67e9
Sattwik Sahu <sahusattwik@gmail.com>
2023-12-13 17:53:07 +0530

    add:firebase auth ui

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.crjpsucuh38"
+    "revision": "0.re5t2sbkdlg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,21 +1,27 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCeEuduMbEOljzZ1jX9M98AdDrCJQZVoU8",
-//   authDomain: "iiserllm.firebaseapp.com",
-//   projectId: "iiserllm",
-//   storageBucket: "iiserllm.appspot.com",
-//   messagingSenderId: "889823312756",
-//   appId: "1:889823312756:web:bd3db5befbaab992e04a21",
-//   measurementId: "G-5V2V7EP1GK",
-// };
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId,
+};
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// export const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+
+// connectAuthEmulator(auth, "http://127.0.0.1:9099");
+
+export default app;
