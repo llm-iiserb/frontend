cd3e5dae79f27f05281e94daa84d1d00b72865a0
Sattwik Sahu <sahusattwik@gmail.com>
2023-12-14 17:59:24 +0530

    fix:no auth text size on mobile reduced

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.a36cra44jo8"
+    "revision": "0.32ort6asm7g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
